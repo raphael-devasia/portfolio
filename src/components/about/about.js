67e7d7fccf89cd9c55294bd023162769cf27b842
raphael-devasia <122820743+raphael-devasia@users.noreply.github.com>
2023-05-19 19:07:14 -0400

    deleted images not needed
    deleted images not needed and resume added

--- src/components/about/about.js
+++ src/components/about/about.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "./about.css";
 import AboutImg from "../../assets/20230405_123224.jpg";
-import CV from "../../assets/about.png";
+import CV from "../../assets/Jay_Dee_Resume.pdf";
 import Info from "./Info";
 
 const About = () => {
